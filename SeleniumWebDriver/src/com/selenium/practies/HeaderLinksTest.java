86f83d31c43093143b50d23f13430abab670d4c4
vasudeva <vasu.584@gmail.com>
2021-05-14 09:20:00 +0530

    Keyword & Wait

--- SeleniumWebDriver/src/com/selenium/practies/HeaderLinksTest.java
+++ SeleniumWebDriver/src/com/selenium/practies/HeaderLinksTest.java
@@ -1,12 +1,16 @@
 package com.selenium.practies;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HeaderLinksTest {
+public class HeaderLinksTest
+{
 
 	public static void main(String[] args) throws Exception
 	{
@@ -19,13 +23,21 @@ public class HeaderLinksTest {
 		
 		driver.manage().window().maximize();
 		
-		Thread.sleep(2000);
+		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		WebDriverWait myWait=new WebDriverWait(driver, 20);
+		
+		
+		
+	//	Thread.sleep(2000);//wait
 		
 		//identify the header & stored in webelement
 		
 		//WebElement header=driver.findElement(By.id("nav-xshop"));
 		
 		//under the header find all the links
+		
+		myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='nav-xshop']/a")));
 		
 		List<WebElement> headerLinks=driver.findElements(By.xpath("//div[@id='nav-xshop']/a"));
 		
